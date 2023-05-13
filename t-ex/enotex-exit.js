9e90b9b90dd1e02be093089093982a66e5e87bc2
Enot-Enot-test <i.is.raccoon228@gmail.com>
2023-05-13 18:06:55 +0300

    Create enotex-exit.js

---
+++ t-ex/enotex-exit.js
@@ -0,0 +1,34 @@
+﻿(function (Scratch) {
+  'use strict';
+
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error('EHOT Exit extension needs to be run unsandboxed');
+  }
+
+  class enotex-exit {
+    getInfo () {
+      return {
+        id: 'enotex-exit',
+        name: 'EHOT Exit',
+        color1: '#3467eb',
+        color2: '#4e7bed',
+        color3: '#2d53b3',
+
+        blocks: [
+          {
+            opcode: 'AndroidClose',
+            text: 'Закртыь Android приложение',
+            blockType: Scratch.BlockType.COMMAND,
+          },
+        ],
+		
+      };
+    }
+
+    AndroidClose {
+      finish();
+    }
+  }
+
+  Scratch.extensions.register(new enotex-exit());
+})(window.Scratch);
